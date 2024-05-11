08eda1de9ac8afa9fd88d801eebe9fe7798e5bdb
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2024-05-11 10:49:43 -0500

    finalizado

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,33 +1,51 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import AmountDisplay from "./AmountDisplay";
+import { useBudget } from "../hooks/useBudget";
+import 'react-circular-progressbar/dist/styles.css';
 
 
 export default function BudgetTracker() {
+
+    const {state, totalExpenses, remainingBudget, dispatch}= useBudget()
+
+    const percentage = +((totalExpenses/state.budget)*100).toFixed(2)
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
-                <img src="../../grafico.jpg" alt="Grafica de gastos" />
+                <CircularProgressbar 
+                    value={percentage}
+                    styles={buildStyles({
+                        pathColor:  percentage>80? '#dc2626':'#3b82f6',
+                        trailColor: '#f5f5f5',
+                        textSize: 8,
+                        textColor: '#3b82f6'
+                    })}
+                    text={`${percentage}% Gastado`}
+                />
             </div>
             <div className="flex flex-col justify-center items-center gap-8">
                 <button
                     type="button"
                     className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                    onClick={()=>dispatch({type: 'reset-app'})}
                 >
                     Resetear App
                 </button>
 
                 <AmountDisplay
                     label="Presupuesto"
-                    amount={300}
+                    amount={state.budget}
                 />
 
                 <AmountDisplay
                     label="Disponible"
-                    amount={200}
+                    amount={remainingBudget}
                 />
 
                 <AmountDisplay
                     label="Gastado"
-                    amount={100}
+                    amount={totalExpenses}
                 />
 
             </div>
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,11 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
 
@@ -14,31 +16,77 @@ export default function ExpenseForm() {
         date: new Date()
     })
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>| ChangeEvent<HTMLSelectElement>)=>{
-        const {name, value}=e.target
+    const [error, setError] = useState('')
+
+    const [previousAmount, setPreviousAmount] = useState(0)
+
+    const { dispatch, state, remainingBudget } = useBudget()
+
+    useEffect(() => {
+        if (state.editingId) {
+            const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]
+            setExpense(editingExpense)
+            setPreviousAmount(editingExpense.amount)
+        }
+    }, [state.editingId])
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target
         const isAmountField = ['amount'].includes(name)
         setExpense({
             ...expense,
-            [name]: isAmountField ? +value: value
+            [name]: isAmountField ? +value : value
         })
-        
+
     }
 
-    const handleChangedate = (value:Value)=>{
+    const handleChangedate = (value: Value) => {
         setExpense({
             ...expense,
-            date:value
+            date: value
         })
-        
+
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (Object.values(expense).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+
+        if (expense.amount - previousAmount > remainingBudget) {
+            setError('Ese gasto se sale del presupuesto')
+            return
+        }
+
+        if (state.editingId) {
+            dispatch({type: 'update-expense', payload: {expense:{id: state.editingId, ...expense}}})
+        } else {
+            dispatch({ type: 'add-expense', payload: { expense } })
+        }
+
+
+        setExpense({
+            amount: 0,
+            expenseName: '',
+            category: '',
+            date: new Date()
+        })
+        setPreviousAmount(0)
+
     }
 
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <legend
                 className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
             >
-                Nuevo gastos
+                {state.editingId ? 'Guardar cambios': 'Nuevo Gasto'}
             </legend>
+
+            {error && <ErrorMessage>{error}</ErrorMessage>}
 
             <div className=" flex flex-col gap-2">
                 <label
@@ -54,6 +102,7 @@ export default function ExpenseForm() {
                     className="bg-slate-100 p-2"
                     name="expenseName"
                     onChange={handleChange}
+                    value={expense.expenseName}
                 />
             </div>
 
@@ -71,6 +120,7 @@ export default function ExpenseForm() {
                     className="bg-slate-100 p-2"
                     name="amount"
                     onChange={handleChange}
+                    value={expense.amount}
                 />
             </div>
 
@@ -86,6 +136,7 @@ export default function ExpenseForm() {
                     className="bg-slate-100 p-2"
                     name="category"
                     onChange={handleChange}
+                    value={expense.category}
                 >
                     <option value="">--Seleciones--</option>
                     {categories.map(category => (
@@ -113,7 +164,7 @@ export default function ExpenseForm() {
 
             <input type="submit"
                 className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-                value="Registrar Gasto" />
+                value={state.editingId ? "Guardar cambios":"Registrar Gasto"} />
 
         </form>
     )
